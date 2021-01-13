4768dd5d7558f7feeefecff12089b781a94741fb
Siim Sepp <siim.sepp@gmail.com>
2021-01-13 13:44:12 +0200

    c

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -5,7 +5,17 @@ const Statistics = () => {
   const context = useContext(Context)
   const { scoreCorrect, numberOfQuestions, startGame, settingsClicked } = context
 
-  const percent = ((scoreCorrect / numberOfQuestions) * 100).toFixed(1)
+  const percent = (scoreCorrect / numberOfQuestions) * 100
+  let status
+  if (percent === 100) {
+    status = 'Geograafia ekspert'
+  } else if (percent > 90) {
+    status = 'Täielik Einstein'
+  } else if (percent > 50) {
+    status = 'Hästi tehtud'
+  } else {
+    status = 'Vajab veel harjutamist'
+  }
 
   return (
     <div>
@@ -14,8 +24,11 @@ const Statistics = () => {
         <li key='1' className='collection-item'>
           <div>Esitatud küsimuste arv: {numberOfQuestions}</div>
         </li>
-        <li key='4' className='collection-item'>
-          <div>Õigete vastuste protsent: {!isNaN(percent) ? percent : 0}</div>
+        <li key='2' className='collection-item'>
+          <div>Õigete vastuste protsent: {!isNaN(percent) ? percent.toFixed(1) : 0}</div>
+        </li>
+        <li key='3' className='collection-item'>
+          <div className='status'>{status}</div>
         </li>
       </ul>
       <button className='btn' onClick={startGame}>
